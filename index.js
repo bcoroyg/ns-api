d460b00d4003f6d0548e9d616a34f0c059a4baac
Brandon Coroy <becgonzalez25@gmail.com>
2022-06-09 16:47:51 -0600

    Conexión a la base de datos Mongo

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/database.js';
+
+const connectionDB = async () =>{
+    await connectDB();
+}
+connectionDB();
 
 const app = express();
 const port = process.env.PORT;
